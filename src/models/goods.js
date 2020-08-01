f7203f4de78514389e0f7e394e5fddaa0629e891
Eun Chong Ju <vevsing@gmail.com>
2020-08-01 12:04:54 +0900

    Update goods.js

--- src/models/goods.js
+++ src/models/goods.js
@@ -1,6 +1,15 @@
 export class Goods {
-    constructor(name, price) {
+    constructor(name, price, number) {
+        this.id = uuidv4();
         this.name = name;
         this.price = price;
+        this.number = number
     }
+}
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
 }
